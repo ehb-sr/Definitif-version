96a6d6b7b4c143defe9cd7301a74631b22dcca30
Yankounza <kenny.de.maeyer@student.ehb.be>
2017-03-26 22:47:47 +0200

    Add verkiezingen onderverdeling content

--- resources/views/verkiezing.blade.php
+++ resources/views/verkiezing.blade.php
@@ -33,37 +33,52 @@
             <img src="{{ asset('img/home/parallax/event-background.jpg') }}" class="bgimages" alt=""/>
         </div>
         <div class="contentwrap">
-
+            <h1>Zet de stap! Word Studentenvertegenwoordiger!</h1>
+            <p>De taak als Studentenvertegenwoordiger is een verantwoordelijkheid die je opneemt naar jou en de
+                studenten van de school toe. Je vertegenwoordigt meer dan 5000 studenten in de verschillende
+                onderwijsraden binnen en buiten onze school. Het is een compleet nieuwe ervaring waardoor je de
+                school beter leert kennen en ervaring verwerft in organiseren, in dialoog gaan met andere partijen,
+                adviseren, etc. Als Studentenvertegenwoordiger laat je het beleid van de school voelen wat er leeft
+                onder de studenten. Jouw stem heeft zeker belang en kan dingen veranderen, ten goede van de school.
+            </p>
         </div>
+
         <div class="photowrap">
             <img src="{{ asset('img/home/parallax/event-background.jpg') }}" class="bgimages" alt=""/>
         </div>
         <div class="contentwrap">
-
+            <div id="titelWrap">
+                <h2>Vacature van een Studentenvertegenwoordiger</h2>
+            </div>
+            <!-- Koppel vehaal -->
         </div>
+
         <div class="photowrap">
             <img src="{{ asset('img/home/parallax/event-background.jpg') }}" class="bgimages" alt=""/>
         </div>
         <div class="contentwrap">
-
+            <!-- Wie zoeken we -->
         </div>
+
         <div class="photowrap">
             <img src="{{ asset('img/home/parallax/event-background.jpg') }}" class="bgimages" alt=""/>
         </div>
         <div class="contentwrap">
-
+            <!-- Wat wordt er van jou verwacht -->
         </div>
+
         <div class="photowrap">
             <img src="{{ asset('img/home/parallax/event-background.jpg') }}" class="bgimages" alt=""/>
         </div>
         <div class="contentwrap">
-
+            <!-- Voordelen -->
         </div>
+
         <div class="photowrap">
             <img src="{{ asset('img/home/parallax/event-background.jpg') }}" class="bgimages" alt=""/>
         </div>
         <div class="contentwrap">
-
+            <!-- Ondersteuning -->
         </div>
     </div>
 </div>
