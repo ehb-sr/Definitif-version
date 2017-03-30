b517242f6d5f98d332b4c75332ee2ef9b910e72b
Yankounza <kenny.de.maeyer@student.ehb.be>
2017-03-30 14:19:07 +0200

    Add content verkiezingen Add css properties

--- resources/views/verkiezing.blade.php
+++ resources/views/verkiezing.blade.php
@@ -24,7 +24,7 @@
 </div>
 
 <div class="content">
-    <div class="sitewrap">
+    <div class="sitewrap" id="verk">
         <div class="contentwrap">
             <h1>Verkiezing</h1>
             <p></p>
@@ -50,7 +50,11 @@
             <div id="titelWrap">
                 <h2>Vacature van een Studentenvertegenwoordiger</h2>
             </div>
-            <!-- Koppel vehaal -->
+            <p>Per departement zoeken we steeds 2 mannen en 2 vrouwen</p>
+            <div>
+                <img src="{{ asset('img/coupleAndCouple.png') }}" alt="" id="imgCouple">
+            </div>
+            <p>De samenstelling van de studentenraad moet gender evenwicht zijn per departement.</p>
         </div>
 
         <div class="photowrap">
@@ -58,6 +62,25 @@
         </div>
         <div class="contentwrap">
             <!-- Wie zoeken we -->
+            <div class="flex">
+                <div class="half">
+                    <h2>Wie zoeken we?</h2>
+                    <!-- vragend manneken img -->
+                </div>
+                <div class="half">
+                    <div>
+                        <p>Ben je geëngageerd, en zet je je graag in voor je medestudenten op de school?</p>
+                    </div>
+                    <div class="bordersTB">
+                        <p>Heb je vernieuwende ideeën over de aanpak in de Erasmushogeschool? Ben je geïnteresseerd in
+                            alles binnen den buiten je campus en heb je hier een mening over? Laat je je mening ook
+                            horen en ben je niet bang om in discussie te gaan?</p>
+                    </div>
+                    <div>
+                        <p>Heb je de nodige skills om dingen te veranderen?</p>
+                    </div>
+                </div>
+            </div>
         </div>
 
         <div class="photowrap">
@@ -65,6 +88,23 @@
         </div>
         <div class="contentwrap">
             <!-- Wat wordt er van jou verwacht -->
+            <div class="flex">
+                <div class="half">
+                    <div>
+                        <p>Je kan tijd maken voor de vergaderingen en verantwoordelijkheden die bij je mandaat horen</p>
+                    </div>
+                    <div class="bordersTB">
+                        <p>Je kan essentiële informatie opslaan en doorspelen naar de Studentenraad.</p>
+                    </div>
+                    <div>
+                        <p>Je weet wat er leeft binnen en buiten je campus</p>
+                    </div>
+                </div>
+                <div class="half">
+                    <h2>Wat wordt er van jou verwacht?</h2>
+                    <!-- manneke -->
+                </div>
+            </div>
         </div>
 
         <div class="photowrap">
@@ -72,6 +112,21 @@
         </div>
         <div class="contentwrap">
             <!-- Voordelen -->
+            <h2>Voordelen</h2>
+            <div class="flex">
+                <div class="quatro bordersR">
+                    <p>Je geniet van het statuut als Studentenvertegenwoordiger, dus mag je lessen overslaan om belangrijke vergaderingen bij te wonen</p>
+                </div>
+                <div class="quatro bordersR">
+                    <p>Je bouwt een interessant netwerk uit, waar je verschillende personen binnen de EhB, VVS of UAB leert kennen</p>
+                </div>
+                <div class="quatro bordersR">
+                    <p>Je verandert dingen ten goede voor je medestudenten, appreciatie zal volgen!</p>
+                </div>
+                <div class="quatro">
+                    <p>Je stimuleert je ervaringen in verschillende belangrijke competenties zoals organiseren, vergaderen, standpunten realiseren, etc</p>
+                </div>
+            </div>
         </div>
 
         <div class="photowrap">
@@ -79,6 +134,27 @@
         </div>
         <div class="contentwrap">
             <!-- Ondersteuning -->
+            <h2>Ondersteuning</h2>
+            <div class="flex">
+                <div class="triple bordersR">
+                    <p>Uigaves die relevant zijn met de Studentenraad worden terugbetaald</p>
+                </div>
+                <div class="triple bordersR">
+                    <p>Je krijgt de steun van andere studentenvertegenwoordigers om projecten uit te werken</p>
+                </div>
+                <div class="triple">
+                    <p>Je hebt de steun van verschillende beleidsadviseurs en medewerkers binnen de Erasmushogeschool</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="photowrap">
+            <img src="{{ asset('img/home/parallax/event-background.jpg') }}" class="bgimages" alt=""/>
+        </div>
+        <div class="contentwrap">
+            <!-- Button naar inschrijvings pagina -->
+            <h2>Heb je interesse om studentenvertegenwoordiger te worden? dien dan je kandidatuur in!</h2>
+            <button>Schrijf je in!</button>
         </div>
     </div>
 </div>
