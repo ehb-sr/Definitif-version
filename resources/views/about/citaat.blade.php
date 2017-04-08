21c7784103fca68cc5b851560b332b2aac5f37fb
Yankounza <kenny.de.maeyer@student.ehb.be>
2017-04-08 16:02:11 +0200

    Make blade view of about

---
+++ resources/views/about/citaat.blade.php
@@ -0,0 +1,8 @@
+<p>Heel wat beslissingen binnen de Erasmushogeschool Brussel hebben te maken met onze belangrijkste stakeholders: de studenten. De studentenraad is dan ook enorm nuttig om in dit proces de studenten een daadkrachtige stem te geven. Dankzij de studentenraad blijven de directie, beleidsmakers, directeurs en docenten eraan herinnerd worden dat er bij het opleiden van jonge professionals een groep centraal blijft staan: de studenten!</p>
+<img src="{{ asset('img/textballon.png') }}" alt="" class="citaatImg">
+<div class="contentTitleLeft">
+    <b>sophie moermean</b>
+    <br>
+    status
+</div>
+<img src="{{ asset('img/unknown.jpeg') }}" alt="" class="PersonImgLeft">
---
+++ resources/views/about/content.blade.php
@@ -0,0 +1,33 @@
+
+
+@include('menu.menu')
+
+<div class="sitewrap">
+    <div class="contentwrap" id="contentAbout">
+        @include('about.about')
+    </div>
+    <div class="photowrap">
+        <img src="{{ asset('img/about/parallax/event-background.jpg') }}" class="bgimages" alt=""/>
+    </div>
+    <div class="contentwrap" id="citaatContent">
+        <h1>Citaten</h1>
+        <!-- FOR LOOP -->
+        @include('about.citaatcontainer')
+    </div>
+
+    <div class="photowrap">
+         <img src="{{ asset('img/home/parallax/agenda-background.jpg') }}" class="bgimages" alt=""/>
+    </div>
+    <div class="contentwrap flex" id="citaatContent">
+        <!-- FOR LOOP -->
+        @include('about.citaatcontainer')
+    </div>
+
+    <div class="photowrap">
+        <img src="{{ asset('img/home/parallax/contact-us2-background.jpg') }}" class="bgimages" alt=""/>
+    </div>
+    <div class="contentwrap flex" id="citaatContent">
+        <!-- FOR LOOP -->
+        @include('about.citaatcontainer')
+    </div>
+</div>
