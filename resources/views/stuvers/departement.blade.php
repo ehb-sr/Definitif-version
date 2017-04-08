82335860ba46158f6985b9969901154c4300f461
Yankounza <kenny.de.maeyer@student.ehb.be>
2017-04-08 23:27:23 +0200

    Make blade view of stuver

---
+++ resources/views/stuvers/departement.blade.php
@@ -0,0 +1,5 @@
+<div class="DesignEnItcontent">
+    <img class="roundrect" src="{{ asset('img/campussen/kaai_stuvers.jpg') }}" width="250" height="250"/>
+</div>
+@include('stuvers.stuverabove')
+@include('stuvers.stuverunder')
