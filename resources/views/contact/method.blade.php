ada1943244e80c2d89385252ab0c36a9e51b4cd0
Yankounza <kenny.de.maeyer@student.ehb.be>
2017-04-08 23:57:25 +0200

    Make blade view of contact

---
+++ resources/views/contact/method.blade.php
@@ -0,0 +1,6 @@
+<div class="cardContact">
+    <img src="{{ asset('img/home/facebook-logo-white.jpg') }}" alt="" class="contentImg top">
+    <img src="{{ asset('img/home/facebook-logo.jpg') }}" alt="" class="contentImg">
+    <img src="{{ asset('img/home/email-icon-white.jpg') }}" alt="" class="contentImg top">
+    <img src="{{ asset('img/home/email-icon.jpg') }}" alt="" class="contentImg">
+</div>
