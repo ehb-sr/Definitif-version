3b524f676210412e1d122e7a81df988a8bd403aa
Yankounza <kenny.de.maeyer@student.ehb.be>
2017-04-27 11:27:43 +0200

    Contact options set

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -1,17 +1,23 @@
 <div class="flex" id="contactContent">
-    <div class="card">
-        <img src="{{ asset('img/home/facebook-logo-white.jpg') }}" alt="" class="contentImg top">
-        <img src="{{ asset('img/home/facebook-logo.jpg') }}" alt="" class="contentImg">
-        <h2>Facebook</h2>
-    </div>
-    <div class="card">
-        <img src="{{ asset('img/home/email-icon-white.jpg') }}" alt="" class="contentImg top">
-        <img src="{{ asset('img/home/email-icon.jpg') }}" alt="" class="contentImg">
-        <h2>Mail</h2>
-    </div>
-    <div class="card">
-        <img src="{{ asset('img/home/form-icon-white.png') }}" alt="" class="contentImg top">
-        <img src="{{ asset('img/home/form-icon.png') }}" alt="" class="contentImg">
-        <h2>Algemeen Contactformulier</h2>
-    </div>
+    <a href="https://www.facebook.com/Studentenraad-Erasmushogeschool-Brussel-1413231898922199/" target="_blank">
+        <div class="card" >
+            <img src="{{ asset('img/home/facebook-logo-white.jpg') }}" alt="" class="contentImg top">
+            <img src="{{ asset('img/home/facebook-logo.jpg') }}" alt="" class="contentImg">
+            <h2>Facebook</h2>
+        </div>
+    </a>
+    <a href="mailto:studentenraad@ehb.be" target="_blank">
+        <div class="card">
+            <img src="{{ asset('img/home/email-icon-white.jpg') }}" alt="" class="contentImg top">
+            <img src="{{ asset('img/home/email-icon.jpg') }}" alt="" class="contentImg">
+            <h2>Mail</h2>
+        </div>
+    </a>
+    <a href="/contact">
+        <div class="card" >
+            <img src="{{ asset('img/home/form-icon-white.png') }}" alt="" class="contentImg top">
+            <img src="{{ asset('img/home/form-icon.png') }}" alt="" class="contentImg">
+            <h2>Algemeen Contactformulier</h2>
+        </div>
+    </a>
 </div>
